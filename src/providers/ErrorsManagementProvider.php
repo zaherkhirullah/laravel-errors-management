47934bc73f036459de21585b53a4bf52d767d301
zahir hayrullah <z.hayrullah@imtilak.net>
2020-04-07 16:25:08 +0300

    fix composer package

--- src/providers/ErrorsManagementProvider.php
+++ src/providers/ErrorsManagementProvider.php
@@ -36,7 +36,7 @@ class ErrorsManagementProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views/errors-management', 'ErrorManagement');
+        $this->loadViewsFrom(__DIR__.'/../resources/views/errors-management', 'ErrorsManagement');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->registerModels();
