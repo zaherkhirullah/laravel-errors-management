40ee033d77924b367d7b6dd57acda38f6ddc1cc9
zahir hayrullah <z.hayrullah@imtilak.net>
2020-04-07 18:04:24 +0300

    update provider

--- src/ErrorsManagementProvider.php
+++ src/ErrorsManagementProvider.php
@@ -22,8 +22,8 @@ class ErrorsManagementProvider extends ServiceProvider
             $this->registerCommands();
         }
 
-        $this->mergeConfigFrom(__DIR__.'/../config/record_errors.php', 'record_errors');
-        $this->mergeConfigFrom(__DIR__.'/../config/adminlte.php', 'adminlte');
+        $this->mergeConfigFrom(__DIR__.'/config/record_errors.php', 'record_errors');
+        $this->mergeConfigFrom(__DIR__.'/config/adminlte.php', 'adminlte');
 
         $this->registerBladeExtensions();
     }
@@ -35,8 +35,8 @@ class ErrorsManagementProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views/errors-management', 'ErrorsManagement');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views/errors-management', 'ErrorsManagement');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->registerModels();
@@ -85,35 +85,35 @@ class ErrorsManagementProvider extends ServiceProvider
         if ($this->isLumen() === false and function_exists('config_path')) { // function not available and 'publish' not relevant in Lumen
 
             $this->publishes(
-                [__DIR__.'/../config/record_errors.php' => config_path('record_errors.php')],
+                [__DIR__.'/config/record_errors.php' => config_path('record_errors.php')],
                 'config'
             );
 
             $timestamp = date('Y_m_d_His', time());
             if (!class_exists('CreateRecordErrorsTable')) {
                 $this->publishes(
-                    [__DIR__.'/../../database/migrations/2020_04_105050_create_record_errors_table.php' => database_path("migrations/{$timestamp}_create_record_errors_table.php")],
+                    [__DIR__.'/../database/migrations/2020_04_105050_create_record_errors_table.php' => database_path("migrations/{$timestamp}_create_record_errors_table.php")],
                     'migrations'
                 );
             }
 
             if (!class_exists('CreateVisitsTable')) {
                 $this->publishes(
-                    [__DIR__.'/../../database/migrations/2020_04_105051_create_visits_table.php' => database_path("migrations/{$timestamp}_create_visits_table.php")],
+                    [__DIR__.'/../database/migrations/2020_04_105051_create_visits_table.php' => database_path("migrations/{$timestamp}_create_visits_table.php")],
                     'migrations'
                 );
             }
 
             $this->publishes(
                 [
-                    __DIR__.'/../resources/views/errors-management' => $this->app->resourcePath('views/vendor/errors-management'),
-                    __DIR__.'/../resources/views/errors'            => $this->app->resourcePath('views/errors'),
+                    __DIR__.'/resources/views/errors-management' => $this->app->resourcePath('views/vendor/errors-management'),
+                    __DIR__.'/resources/views/errors'            => $this->app->resourcePath('views/errors'),
                 ],
                 'errors-management-views'
             );
 
             $this->publishes(
-                [__DIR__.'/../providers/ErrorsManagementProvider.php' => app_path('/Providers/ErrorsManagementProvider.php')],
+                [__DIR__.'/ErrorsManagementProvider.php' => app_path('/Providers/ErrorsManagementProvider.php')],
                 'providers'
             );
         }
@@ -139,7 +139,7 @@ class ErrorsManagementProvider extends ServiceProvider
      */
     public function registerHelpers()
     {
-        $helpers = glob(__DIR__.'/../Helpers/*.php');
+        $helpers = glob(__DIR__.'/Helpers/*.php');
         foreach ($helpers as $helper) {
             if (file_exists($helper)) {
                 require_once $helper;
