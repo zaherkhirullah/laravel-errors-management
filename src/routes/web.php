0d57c6075f3514f5e38a5ab1f113d49489849d6b
Zaher Khirullah <zahir.hayrallah@gmail.com>
2020-04-08 13:35:57 +0000

    Apply fixes from StyleCI

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-//    'middleware' => $middleware,
+    //    'middleware' => $middleware,
     'prefix'    => 'errors-management',
     'namespace' => 'Hayrullah\ErrorsManagement\Http\Controllers',
 ], function () {
-
     Route::get('/', 'RecordErrorController@dashboard')->name('error-records.dashboard');
     Route::get('records/{code}', 'RecordErrorController@index')->name('errors_management:records');
     Route::get('records/{code}/{id}', 'RecordErrorController@show')->name('errors_management:show');
