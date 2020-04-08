2dfafdceac245d28fdef612657e00838964e42af
zahir hayrullah <z.hayrullah@imtilak.net>
2020-04-08 16:50:07 +0300

    fix public path

--- src/resources/views/errors-management/index.blade.php
+++ src/resources/views/errors-management/index.blade.php
@@ -43,7 +43,7 @@
 @stop
 
 @section('js')
-    <script src="{{asset('js/global.js')}}"></script>
+    <script src="{{asset('vendor/errors-management/js/global.js')}}"></script>
     <script type="text/javascript">
         $(document).ready(function () {
                 <?php $TrashParam = has_trash_param()?>
